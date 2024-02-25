f2ef59970bd22cd676b5b27308b1b8b7ac54b5e9
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2024-02-26 10:57:48 +1300

    added not yet implemented message to category controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -2,34 +2,34 @@ const asyncHandler = require('express-async-handler');
 
 
 exports.category_list = asyncHandler(async (req, res) => {
-    res.send("Page for Category list")
+    res.send("NOT_YET_IMPLEMENTED: Page for Category list")
 }
 );
 
 exports.detail_get = asyncHandler(async (req, res) => {
-    res.send(`Detail Page for Category: ${req.params.category_id}`)
+    res.send(`NOT_YET_IMPLEMENTED: Detail Page for Category: ${req.params.category_id}`)
 });
 
 exports.create_get = asyncHandler(async (req, res) => {
-    res.send(`Create Category page`)
+    res.send(`NOT_YET_IMPLEMENTED: Create Category page`)
 });
 
 exports.create_post = asyncHandler(async (req, res) => {
-    res.send(`Create Category Post Request Response`)
+    res.send(`NOT_YET_IMPLEMENTED: Create Category Post Request Response`)
 });
 
 exports.update_get = asyncHandler(async (req, res) => {
-    res.send(`Update page for ${req.params.category_id}`)
+    res.send(`NOT_YET_IMPLEMENTED: Update page for ${req.params.category_id}`)
 });
 
 exports.update_post = asyncHandler(async (req, res) => {
-    res.send(`Update Post Requst response for ${req.params.category_id}`)
+    res.send(`NOT_YET_IMPLEMENTED: Update Post Requst response for ${req.params.category_id}`)
 });
 
 exports.delete_get = asyncHandler(async (req, res) => {
-    res.send(`Delete Page for  ${req.params.category_id}`)
+    res.send(`NOT_YET_IMPLEMENTED: Delete Page for  ${req.params.category_id}`)
 });
 
 exports.delete_post = asyncHandler(async (req, res) => {
-    res.send(`Delete Post Requst response for ${req.params.item_id}`)
+    res.send(`NOT_YET_IMPLEMENTED: Delete Post Requst response for ${req.params.item_id}`)
 });
