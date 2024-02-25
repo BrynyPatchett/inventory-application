b904d9defe724937f60013542eb5db7ecc1eb5ff
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2024-02-26 10:28:34 +1300

    added placeholder Item controller functions

---
+++ controllers/itemController.js
@@ -0,0 +1,19 @@
+const asyncHandler = require('express-async-handler');
+
+exports.index = null;
+
+exports.item_list = null;
+
+exports.detail_get = null;
+
+exports.create_get = null;
+
+exports.create_post = null;
+
+exports.update_get = null;
+
+exports.update_post = null;
+
+exports.delete_get = null;
+
+exports.delete_post = null;
